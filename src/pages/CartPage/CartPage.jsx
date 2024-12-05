889aa6fc77ab5cff8ea8da18b9897d160c211146
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-12-06 01:48:47 +0200

    2.6.4

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -3,31 +3,31 @@ import { removeItemFromCart, clearCart } from "../../redux/cart/slice";
 import css from "./CartPage.module.css";
 
 export default function CartPage() {
-  const cart = useSelector((state) => state.cart.items); // Получаем корзину из глобального состояния
+  const cart = useSelector((state) => state.cart.items);
   const dispatch = useDispatch();
 
   const handleRemoveFromCart = (id) => {
-    dispatch(removeItemFromCart(id)); // Удаляем товар по id
+    dispatch(removeItemFromCart(id));
   };
 
   const handleCheckout = () => {
-    dispatch(clearCart()); // Очищаем корзину после покупки
-    alert("Спасибо за покупку!");
+    dispatch(clearCart());
+    alert("Thank you for your purchase!");
   };
 
   const getTotalPrice = () => {
     return cart.reduce((total, item) => {
       const price = parseFloat(item.price.replace("$", "")) || 0;
-      return total + price * item.quantity; // Учитываем количество
+      return total + price * item.quantity;
     }, 0);
   };
 
   return (
     <div className={css.container}>
       <div className={css.cart}>
-        <h2>Корзина</h2>
+        <h2>Shopping Cart</h2>
         {cart.length === 0 ? (
-          <p>Ваша корзина пуста.</p>
+          <p>Your cart is empty.</p>
         ) : (
           <ul>
             {cart.map((item) => (
@@ -40,7 +40,7 @@ export default function CartPage() {
                   onClick={() => handleRemoveFromCart(item.id)}
                   className={css.removeButton}
                 >
-                  Уменьшить
+                  Remove
                 </button>
               </li>
             ))}
@@ -49,10 +49,10 @@ export default function CartPage() {
         {cart.length > 0 && (
           <>
             <div className={css.total}>
-              Общая стоимость: ${getTotalPrice().toFixed(2)}
+              Total Price: ${getTotalPrice().toFixed(2)}
             </div>
             <button className={css.checkoutButton} onClick={handleCheckout}>
-              Оформить заказ
+              Checkout
             </button>
           </>
         )}
