3eefd3a87ea29776ac6c2ca177b969dc246bb375
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-12-06 02:59:26 +0200

    2.6.5

--- src/components/Layout/AppBar/AuthNav/AuthNav.jsx
+++ src/components/Layout/AppBar/AuthNav/AuthNav.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { FaSignInAlt, FaUserPlus } from "react-icons/fa"; // Добавляем иконки для SignIn и SignUp
 import clsx from "clsx";
 import css from "./AuthNav.module.css";
-
-import { FaBars, FaTimes } from "react-icons/fa";
 
 export default function AuthNav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -25,29 +25,35 @@ export default function AuthNav() {
         onClick={toggleMenu}
         aria-label="Toggle menu"
       >
-        {isMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+        {isMenuOpen ? (
+          <FaTimes className={css.icon} />
+        ) : (
+          <FaBars className={css.icon} />
+        )}
       </button>
 
       <div
         className={clsx(css["menu-container"], isMenuOpen && css["is-open"])}
       >
         <ul className={css["menu-list"]}>
-          <li className={css["menu-item"]}>
+          <li className={css.li}>
             <NavLink
               to="/login"
               className={css["menu-link"]}
               onClick={closeMenu}
             >
-              SignIn
+              <FaSignInAlt className={css.icon} />
+              <span className={css.text}>Sign In</span>
             </NavLink>
           </li>
-          <li className={css["menu-item"]}>
+          <li className={css.li}>
             <NavLink
               to="/register"
               className={css["menu-link"]}
               onClick={closeMenu}
             >
-              SignUp
+              <FaUserPlus className={css.icon} />
+              <span className={css.text}>Sign Up</span>
             </NavLink>
           </li>
         </ul>
--- src/components/Layout/AppBar/Navigation/Navigator.jsx
+++ src/components/Layout/AppBar/Navigation/Navigator.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
-
+import { FaHome, FaImages, FaShoppingCart } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import clsx from "clsx";
 import css from "./Navigation.module.css";
 
@@ -7,25 +8,37 @@ const getNavLinkClass = ({ isActive }) =>
   clsx(css.link, isActive && css.isActive);
 
 export default function Navigation() {
+  const cartItems = useSelector((state) => state.cart.items);
+  const totalItems = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
   return (
-    <>
+    <ul className={css.ul}>
       <li className={css.li}>
         <NavLink to="/" className={getNavLinkClass}>
-          Home
+          <FaHome className={css.icon} />
+          <span className={css.text}>Home</span>
         </NavLink>
       </li>
 
       <li className={css.li}>
         <NavLink to="/gallery" className={getNavLinkClass}>
-          Gallery
+          <FaImages className={css.icon} />
+          <span className={css.text}>Gallery</span>
         </NavLink>
       </li>
 
       <li className={css.li}>
         <NavLink to="/cart" className={getNavLinkClass}>
-          Cart
+          <FaShoppingCart className={css.icon} />
+          {totalItems > 0 && (
+            <span className={css.cartBadge}>{totalItems}</span>
+          )}
+          <span className={css.text}>Cart</span>
         </NavLink>
       </li>
-    </>
+    </ul>
   );
 }
