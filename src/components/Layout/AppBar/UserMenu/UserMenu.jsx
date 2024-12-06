d6037def4fccaa1a87e4f88b32689b5cdad87efa
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-12-06 03:43:08 +0200

    2.6.6

--- src/components/Layout/AppBar/UserMenu/UserMenu.jsx
+++ src/components/Layout/AppBar/UserMenu/UserMenu.jsx
@@ -1,20 +1,48 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../../../redux/auth/operations";
+import { FaSignOutAlt } from "react-icons/fa";
+import Modal from "react-modal";
 import css from "./UserMenu.module.css";
+
+Modal.setAppElement("#root");
 
 export default function UserMenu() {
   const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const onLogout = () => {
+  const handleLogout = () => {
     dispatch(logout());
+    closeModal();
   };
 
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
-    <li className={css.userMenu}>
-      <span className={css.greeting}>Welcome, User!</span>
-      <button onClick={onLogout} className={css.logoutButton}>
-        Logout
+    <li className={css.li}>
+      <button onClick={openModal} className={css.link}>
+        <FaSignOutAlt className={css.icon} />
+        <span className={css.text}>Logout</span>
       </button>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        className={css.modal}
+        overlayClassName={css.overlay}
+      >
+        <h2 className={css.modalTitle}>Confirm Logout</h2>
+        <p className={css.modalText}>Are you sure you want to logout?</p>
+        <div className={css.modalButtons}>
+          <button onClick={handleLogout} className={css.confirmButton}>
+            Yes, Logout
+          </button>
+          <button onClick={closeModal} className={css.cancelButton}>
+            Cancel
+          </button>
+        </div>
+      </Modal>
     </li>
   );
 }
