359d5c03c5eaaa3f16434fb4dfb99ae7a352a6e6
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-12-06 03:46:24 +0200

    2.6.6

--- src/pages/NotFounderPage/NotFounderPage.jsx
+++ src/pages/NotFounderPage/NotFounderPage.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import css from "./NotFounderPage.module.css";
 
-export default function NotFoundPage() {
+export default function NotFounderPage() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>The page</p>
-      <Link to="/">Go to Home</Link>
+    <div className={css.container}>
+      <h1 className={css.title}>404</h1>
+      <p className={css.message}>
+        Oops! The page you are looking for does not exist.
+      </p>
+      <Link to="/" className={css.link}>
+        Go to Home
+      </Link>
     </div>
   );
 }
